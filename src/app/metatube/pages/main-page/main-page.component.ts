d933452986da5ab0d3849172fd58cba4ad5147b1
DESKTOP-2MV8IEM\ramon <ardestino@gmail.com>
2022-09-18 14:03:07 -0500

    codigo base exportacion pdf

--- src/app/metatube/pages/main-page/main-page.component.ts
+++ src/app/metatube/pages/main-page/main-page.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import charts from '../../../../@youpez/data/charts'
+
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from "pdfmake/build/vfs_fonts";
+const htmlToPdfmake = require("html-to-pdfmake");
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-main-page',
@@ -13,6 +18,17 @@ export class MainPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChild('pdfTable')
+  pdfTable!: ElementRef;
+  
+  public downloadAsPDF() {
+    const pdfTable = this.pdfTable.nativeElement;
+    var html = htmlToPdfmake(pdfTable.innerHTML);
+    const documentDefinition = { content: html };
+    pdfMake.createPdf(documentDefinition).download(); 
+     
   }
 
 }
