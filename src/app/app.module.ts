5d670a701a1437ea1df757a706daa865e7a38fe1
DESKTOP-2MV8IEM\ramon <ardestino@gmail.com>
2022-10-15 12:17:49 -0500

    Limpieza del modulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,17 @@
 import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 import {FlexLayoutModule} from '@angular/flex-layout'
-//import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
-//import {NgxMdModule} from "ngx-md"
 
-//import {CoreModule} from "./core/core.module"
-//import {SharedModule} from './shared/shared.module'
 import {MetatubeModule} from "./metatube/metatube.module";
 import { AuthModule } from "./auth/auth.module";
 import {ApiModule} from "../api/api.module";
 
-import {
-  HttpClientModule,
-  //HttpEvent,
-  //HttpInterceptor,
-  //HttpHandler,
-  //HttpRequest,
-  //HTTP_INTERCEPTORS
-} from '@angular/common/http'
-
+import { HttpClientModule } from '@angular/common/http'
 import {AppRoutingModule} from './app.routing'
 import {AppComponent} from './app.component'
-import {StoreModule} from '@ngrx/store'
-//import {reducers, metaReducers} from './store/app.reducers'
 import {StoreDevtoolsModule} from '@ngrx/store-devtools'
 import {environment} from '../environments/environment'
-//import {EffectsModule} from '@ngrx/effects'
-//import {AppEffects} from './store/app.effects'
 import { Configuration, ConfigurationParameters } from "../api/index";
-
 
 const configFactory = () => {
   const configParams : ConfigurationParameters = {
@@ -39,24 +22,16 @@ const configFactory = () => {
   return new Configuration(configParams);
 }
 
-
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    //BrowserAnimationsModule,
     AppRoutingModule,
     FlexLayoutModule,
-    //CoreModule,
     HttpClientModule,
-    //SharedModule,
-    //StoreModule.forRoot({}),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
-    //EffectsModule.forRoot([AppEffects]),
-    //NgxMdModule.forRoot(),
     MetatubeModule,
     AuthModule,
     ApiModule.forRoot(configFactory),
