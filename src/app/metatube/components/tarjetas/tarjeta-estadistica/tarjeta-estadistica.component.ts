d3119d278f778d8f7900569732372d377b2b8d49
DESKTOP-2MV8IEM\ramon <ardestino@gmail.com>
2022-10-18 20:42:08 -0500

    Creacion de la tarjeta de estadisticas del canal

--- src/app/metatube/components/tarjetas/tarjeta-estadistica/tarjeta-estadistica.component.ts
+++ src/app/metatube/components/tarjetas/tarjeta-estadistica/tarjeta-estadistica.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ListItem } from 'carbon-components-angular';
 
 @Component({
   selector: 'app-tarjeta-estadistica',
@@ -10,6 +11,49 @@ export class TarjetaEstadisticaComponent implements OnInit {
   @Input() titulo:string;
   @Input() subtitulo:string;
   @Input() numero;
+
+  public comboboxItems : ListItem[] = [
+    {
+      content: "Ultimos 7 dias",
+      selected: true
+    },
+    {
+      content: "Ultimo Mes",
+      selected: false
+    },
+    {
+      content: "Ultimo Año",
+      selected: false
+    }
+  ]
+
+  public subscriptions = [
+    {
+      month: 'videos',
+      count: '-111,333',
+      up: false,
+    },
+    {
+      month: 'vistas',
+      count: '233,123',
+      up: true,
+    },
+    {
+      month: 'subscriptores',
+      count: '543,854',
+      up: true,
+    },
+    {
+      month: 'Tiempo Visualizacion',
+      count: '-99,112',
+      up: false,
+    },
+    {
+      month: 'Retencion',
+      count: '678,112',
+      up: true,
+    }
+  ]
 
   constructor() {
 
