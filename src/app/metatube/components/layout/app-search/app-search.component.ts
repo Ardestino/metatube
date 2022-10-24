13a35c956912bba36c348b6962f7d147800c1227
DESKTOP-2MV8IEM\ramon <ardestino@gmail.com>
2022-10-24 16:42:22 -0500

    Se cargaron acciones basicas de buscador

--- src/app/metatube/components/layout/app-search/app-search.component.ts
+++ src/app/metatube/components/layout/app-search/app-search.component.ts
@@ -10,9 +10,16 @@ import {
 } from '@angular/core'
 
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { AppState } from 'src/app/metatube/store';
 import * as SearchActions from "src/app/metatube/store/search/search.actions";
 
+interface SearchResult{
+    channelId : string
+    title : string
+    description : string
+    thumbnail : string
+  }
 
 @Component({
   selector: 'youpez-search',
@@ -24,15 +31,25 @@ export class AppSearchComponent implements OnInit, AfterViewInit {
   @HostListener('window:keyup', ['$event']) keyEvent(event: KeyboardEvent) {
     if (event.key === 'Escape') {
       this.onClose()
+    } if (event.key === 'Enter'){
+      this.store.dispatch(SearchActions.iniciarBusqueda());
+      this.onSearch();
     }
   }
 
   @ViewChild('searchElement') searchElement: ElementRef
 
-  public loading: boolean = true
+  public loading$ : Observable<boolean>;
+  public search_text$ : Observable<string>;
+  public search_result$ : Observable<SearchResult[]>;
+  public selected_result$ : Observable<SearchResult>;
+
 
   constructor(private store: Store<AppState>) {
-
+    this.loading$ = store.select(state => state.search.loading);
+    this.search_text$ = store.select(state => state.search.search_term);
+    this.search_result$ = store.select(state => state.search.search_result);
+    this.selected_result$ = store.select(state => state.search.selected_result);
   }
 
   ngOnInit(): void {
@@ -41,10 +58,22 @@ export class AppSearchComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     setTimeout(() => {
+      this.searchElement.nativeElement.focus();
+      this.store.dispatch(SearchActions.iniciarBusqueda());
+    }, 200)
+    setTimeout(() => {
+      this.store.dispatch(SearchActions.busquedaExitosa());
+    }, 700)
+  }
+
+  onSearch(): void {
+    setTimeout(() => {
+      // TODO: Dar el foco al buscador al momento de que es visible
       this.searchElement.nativeElement.focus()
     }, 200)
     setTimeout(() => {
-      this.loading = false
+      this.store.dispatch(SearchActions.busquedaExitosa());
+      this.store.dispatch(SearchActions.resultadoSeleccionado());
     }, 700)
   }
 
--- src/app/metatube/store/search/search.actions.ts
+++ src/app/metatube/store/search/search.actions.ts
@@ -2,3 +2,7 @@ import { createAction, props } from '@ngrx/store';
 
 export const mostrarBuscador = createAction('[Search] Monstrar Buscador');
 export const ocultarBuscador = createAction('[Search] Ocultar Buscador');
+export const iniciarBusqueda = createAction('[Search] Iniciar Busqueda'); // TODO: Agregar parametro
+export const busquedaExitosa = createAction('[Search] Busqueda Exitosa');
+export const busquedaErronea = createAction('[Search] Busqueda Erronea');
+export const resultadoSeleccionado = createAction('[Search] Resultado seleccionado');
