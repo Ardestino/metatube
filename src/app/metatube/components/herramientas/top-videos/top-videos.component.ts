c67c6ce80009c92d19e93fe411212e99422a81e5
DESKTOP-2MV8IEM\ramon <ardestino@gmail.com>
2022-11-13 01:35:23 -0600

    Se agregaron los ultimos 10 videos del canal con sus datos

--- src/app/metatube/components/herramientas/top-videos/top-videos.component.ts
+++ src/app/metatube/components/herramientas/top-videos/top-videos.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ListItem } from 'carbon-components-angular';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/metatube/store';
+import { Observable } from 'rxjs';
+import { VideoService, Channel, Video } from 'src/api';
 
 @Component({
   selector: 'app-top-videos',
@@ -7,38 +11,6 @@ import { ListItem } from 'carbon-components-angular';
   styleUrls: ['./top-videos.component.scss']
 })
 export class TopVideosComponent implements OnInit {
-
-  miniChart2 = {
-    tooltip: {
-      trigger: 'item',
-      formatter: '{a} <br/>{b} : {c} ({d}%)'
-    },
-    legend: {
-      show: false,
-    },
-    series: [
-      {
-        name: 'Browsers',
-        type: 'pie',
-        radius: '75%',
-        center: ['50%', '50%'],
-        data: [
-          {value: 335, name: 'Explorer'},
-          {value: 310, name: 'Firefox'},
-          {value: 234, name: 'Edge'},
-          {value: 135, name: 'Safari'},
-          {value: 1548, name: 'Chrome'}
-        ],
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: 'rgba(0, 0, 0, 0.5)'
-          }
-        }
-      }
-    ]
-  }
 
   public comboboxItems : ListItem[] = [
     {
@@ -55,29 +27,15 @@ export class TopVideosComponent implements OnInit {
     }
   ]
 
-  videos = [
-    {
-      url_video : "https://www.youtube.com/watch?v=TeB7BucZlWM&ab_channel=Black%26WhiteMusic",
-      thumbnail_url : "https://i.ytimg.com/vi/TeB7BucZlWM/hqdefault_live.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDcRgdUqWHSozu5bi0AcYQJBIgp2Q",
-      nombre_video : "MUSICA MÁS ESCUCHADAS 2022 - Famosas canciones de música extranjera que la gente busca en 2022 #84",
-      sentimientos : "",
-      comentarios : "319",
-      vistas : "3.1M",
-      likes : "114230",
-      tiempo_visualizacion : "10000"
-    },{
-      url_video : "https://www.youtube.com/watch?v=TeB7BucZlWM&ab_channel=Black%26WhiteMusic",
-      thumbnail_url : "https://i.ytimg.com/vi/TeB7BucZlWM/hqdefault_live.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDcRgdUqWHSozu5bi0AcYQJBIgp2Q",
-      nombre_video : "MUSICA MÁS ESCUCHADAS 2022 - Famosas canciones de música extranjera que la gente busca en 2022 #84",
-      sentimientos : "",
-      comentarios : "319",
-      vistas : "3.1M",
-      likes : "114230",
-      tiempo_visualizacion : "10000"
-    },
-  ]
+  videos : Video[];
 
-  constructor() { }
+  constructor(private store: Store<AppState>,private videoApi : VideoService) {
+    this.store.select(state => state.proyectos.canal_seleccionado).subscribe((channel)=>{
+      videoApi.videoVideosByChannelList(channel.id).subscribe(videos =>{
+        this.videos = videos;
+      })
+    })
+   }
 
   ngOnInit(): void {
   }
